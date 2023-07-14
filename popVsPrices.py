62d23a732efab90c0655f34de977d0c3684322dc
RunDMC2 <137807504+RunDMC2@users.noreply.github.com>
2023-07-13 20:04:37 -0400

    Update and rename test.py to popVsPrices.py

---
+++ popVsPrices.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as pt
+
+spamreader = pd.read_csv('/Users/serkanaymaz/Downloads/world_pop.csv', index_col = 'Country Name')
+
+world_pop = spamreader.loc['World'].values.tolist()
+world_pop = world_pop[3:-1]
+
+print(world_pop)
+
+years = []
+for i in range(1960, 2023):
+    years.append(i)
+  
+print(years)
+print(len(world_pop))
+print(len(years))
+
+pt.plot(years, world_pop)
+pt.title("World Population From 1960-2022")
+pt.xlabel("Year")
+pt.ylabel("World Population (in billions)")
+
+pt.show()
